28c917cfe9c9d179a8dc5cceab479f4ddb6f967c
shanderb <s.hander8910@hotmail.com>
2025-08-23 16:02:45 -0300

    feat/migracao-de-static-para-angular - Criado componente "metodologia"

---
+++ brasil-transparente/src/app/pages/metodologia/metodologia.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metodologia',
+  imports: [],
+  templateUrl: './metodologia.component.html',
+  styleUrl: './metodologia.component.scss',
+  standalone: true
+})
+export class MetodologiaComponent {
+
+}
