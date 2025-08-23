a50bd3978fca32f869bb5194f577ad68cf368f56
shanderb <s.hander8910@hotmail.com>
2025-08-23 19:26:32 -0300

    feat/migracao-de-static-para-angular - Ajuste do arquivo de environment para evitar duplicação de código.

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,4 +1,34 @@
 export const environment = {
-    //TODO adicionar API para rodar local
-      apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app'
+  //TODO adicionar API para rodar local
+  apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app',
+  estados: [
+    { nome: 'Acre', imagem: 'images/estados/acre.png', id: 0, ativo: false },
+    { nome: 'Alagoas', imagem: 'images/estados/alagoas.png', id: 0, ativo: false },
+    { nome: 'Amapá', imagem: 'images/estados/amapa.png', id: 0, ativo: false },
+    { nome: 'Amazonas', imagem: 'images/estados/amazonas.png', id: 4, ativo: true },
+    { nome: 'Bahia', imagem: 'images/estados/bahia.png', id: 3, ativo: true },
+    { nome: 'Ceará', imagem: 'images/estados/ceara.png', id: 0, ativo: false },
+    { nome: 'Distrito Federal', imagem: 'images/estados/distrito_federal.png', id: 0, ativo: false },
+    { nome: 'Espírito Santo', imagem: 'images/estados/espirito_santo.png', id: 0, ativo: false },
+    { nome: 'Goiás', imagem: 'images/estados/goias.png', id: 0, ativo: false },
+    { nome: 'Maranhão', imagem: 'images/estados/maranhao.png', id: 0, ativo: false },
+    { nome: 'Mato Grosso', imagem: 'images/estados/mato_grosso.png', id: 0, ativo: false },
+    { nome: 'Mato Grosso do Sul', imagem: 'images/estados/mato_grosso_do_sul.png', id: 0, ativo: false },
+    { nome: 'Minas Gerais', imagem: 'images/estados/minas_gerais.png', id: 0, ativo: false },
+    { nome: 'Pará', imagem: 'images/estados/para.png', id: 0, ativo: false },
+    { nome: 'Paraíba', imagem: 'images/estados/paraiba.png', id: 6, ativo: true },
+    { nome: 'Paraná', imagem: 'images/estados/parana.png', id: 0, ativo: false },
+    { nome: 'Pernambuco', imagem: 'images/estados/pernambuco.png', id: 0, ativo: false },
+    { nome: 'Piauí', imagem: 'images/estados/piaui.png', id: 0, ativo: false },
+    { nome: 'Rio de Janeiro', imagem: 'images/estados/rio_de_janeiro.png', id: 0, ativo: false },
+    { nome: 'Rio Grande do Norte', imagem: 'images/estados/rio_grande_do_norte.png', id: 0, ativo: false },
+    { nome: 'Rio Grande do Sul', imagem: 'images/estados/rio_grande_do_sul.png', id: 2, ativo: true },
+    { nome: 'Rondônia', imagem: 'images/estados/rondonia.png', id: 0, ativo: false },
+    { nome: 'Roraima', imagem: 'images/estados/roraima.png', id: 0, ativo: false },
+    { nome: 'Santa Catarina', imagem: 'images/estados/santa_catarina.png', id: 0, ativo: false },
+    { nome: 'São Paulo', imagem: 'images/estados/sao_paulo.png', id: 5, ativo: true },
+    { nome: 'Sergipe', imagem: 'images/estados/sergipe.png', id: 0, ativo: false },
+    { nome: 'Tocantins', imagem: 'images/estados/tocantins.png', id: 0, ativo: false },
+    { nome: 'União Federal', imagem: 'images/estados/uniao.png', id: 1, ativo: true }
+  ]
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,3 +1,6 @@
-export const environment = {
+import { environment as env } from './environment.development';
+
+export const environment: typeof env = {
+      ...env,
       apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app'
 };
