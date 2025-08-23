ba61752b56a79765e164a1180f738e070293c27f
shanderb <s.hander8910@hotmail.com>
2025-08-23 15:50:45 -0300

    feat/migracao-de-static-para-angular - Adicionado arquivos de config para componente.

--- brasil-transparente/src/app/app.ts
+++ brasil-transparente/src/app/app.ts
@@ -1,11 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./core/header/header.component";
+import { FooterComponent } from "./core/footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
+  standalone: true
 })
 export class App {
   protected readonly title = signal('brasil-transparente');
