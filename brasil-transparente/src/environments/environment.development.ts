7c2ec55c36277f6c2041fbf195c238ddeddb8514
shanderb <s.hander8910@hotmail.com>
2025-08-23 15:50:14 -0300

    feat/migracao-de-static-para-angular - Adicionado arquivos de environment.

---
+++ brasil-transparente/src/environments/environment.development.ts
@@ -0,0 +1,4 @@
+export const environment = {
+    //TODO adicionar API para rodar local
+      apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app'
+};
---
+++ brasil-transparente/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+      apiBase: 'https://brasil-transparente-api-492194254445.us-central1.run.app'
+};
