bfb4cd1106630795d33e8f0a869ef8932b914f71
shanderb <s.hander8910@hotmail.com>
2025-08-23 16:02:27 -0300

    feat/migracao-de-static-para-angular - Criado componente "origem"

---
+++ brasil-transparente/src/app/pages/origem/origem.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-origem',
+  imports: [],
+  templateUrl: './origem.component.html',
+  styleUrl: './origem.component.scss',
+  standalone: true
+})
+export class OrigemComponent {
+
+}
