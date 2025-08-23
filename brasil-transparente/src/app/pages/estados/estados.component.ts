c17b68922a6ee838faf5380b2e74ba7086ce5aff
shanderb <s.hander8910@hotmail.com>
2025-08-23 16:03:17 -0300

    feat/migracao-de-static-para-angular - Criado componente "estados"

---
+++ brasil-transparente/src/app/pages/estados/estados.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estados',
+  imports: [],
+  templateUrl: './estados.component.html',
+  styleUrl: './estados.component.scss',
+  standalone: true
+})
+export class EstadosComponent {
+
+}
