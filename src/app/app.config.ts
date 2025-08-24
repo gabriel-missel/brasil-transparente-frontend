f2f9d318f9c998e413304c7dc98ed959fd67c269
shanderb <s.hander8910@hotmail.com>
2025-08-24 11:04:57 -0300

    feat/migracao-de-static-para-angular - Adicionado locale padrão para todo o sistema.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,10 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
+registerLocaleData(ptBr)
 
 export const appConfig: ApplicationConfig = {
   providers: [
